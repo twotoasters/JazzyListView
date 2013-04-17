7ec17f4fbaada75047814c98ba88585639d879ff
James Barr <james@twotoasters.com>
2013-04-17 16:25:35 -0400

    Pass the onScroll event onto any additional listeners besides the list itself

--- library/src/com/twotoasters/jazzylistview/JazzyListView.java
+++ library/src/com/twotoasters/jazzylistview/JazzyListView.java
@@ -3,13 +3,10 @@ package com.twotoasters.jazzylistview;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Point;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
@@ -19,192 +16,191 @@ import com.nineoldandroids.view.ViewPropertyAnimator;
 
 public class JazzyListView extends ListView implements OnScrollListener {
 
-	public enum TransitionEffect {
-		Standard,
-		Grow,
-		Curl,
-		Wave,
-		Flip,
-		Helix,
-		Fan,
-		Zipper,
+    public enum TransitionEffect {
+        Standard,
+        Grow,
+        Curl,
+        Wave,
+        Flip,
+        Helix,
+        Fan,
+        Zipper,
 		Fade
-	}
+    }
 
-	private static final String TAG = "Jazzy";
+    private static final String TAG = "Jazzy";
 
-	private static final boolean IS_AT_LEAST_HC =
-			Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+    private static final boolean IS_AT_LEAST_HC =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
 
-	private static final boolean IS_AT_LEAST_ICS =
-			Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+    public static final int DURATION = 600;
+    public static final int OPAQUE = 255, TRANSPARENT = 0;
 
-	public static final int DURATION = 600;
-	public static final int OPAQUE = 255, TRANSPARENT = 0;
+    private TransitionEffect mEffect = TransitionEffect.Standard;
+    private boolean mIsScrolling = false;
+    private int mFirstVisibleItem = -1;
+    private int mLastVisibleItem = -1;
 
-	private TransitionEffect mEffect = TransitionEffect.Standard;
-	private boolean mIsScrolling = false;
-	private int mFirstVisibleItem = -1;
-	private int mLastVisibleItem = -1;
+    private OnScrollListener mAdditionalOnScrollListener;
 
-	public JazzyListView(Context context) {
-		this(context, null);
-	}
+    public JazzyListView(Context context) {
+        this(context, null);
+    }
 
-	public JazzyListView(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
-	}
+    public JazzyListView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
 
-	public JazzyListView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
+    public JazzyListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
 
-		setOnScrollListener(this);
+        super.setOnScrollListener(this); // call super's method to actually register this list as the listener
 
-		TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.JazzyListView);
-		String strEffect = null;
-		try {
-	        strEffect = a.getString(R.styleable.JazzyListView_effect);
-	        if(strEffect != null) {
-	        TransitionEffect effect = TransitionEffect.valueOf(strEffect);
-	        setTransitionEffect(effect);
-	        }
-		} catch (IllegalArgumentException e) {
-			Log.w(TAG, "Invalid jazzy list view transition effect: " + strEffect);
-		}
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.JazzyListView);
+        String strEffect = null;
+        try {
+            strEffect = a.getString(R.styleable.JazzyListView_effect);
+            if (strEffect != null) {
+                TransitionEffect effect = TransitionEffect.valueOf(strEffect);
+                setTransitionEffect(effect);
+            }
+        } catch (IllegalArgumentException e) {
+            Log.w(TAG, "Invalid jazzy list view transition effect: " + strEffect);
+        }
         a.recycle();
-	}
+    }
 
-	@Override
-	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-		Log.v(TAG, String.format("onScroll() - [firstVis: %d, visItemCount: %d, totItemCount: %d]", firstVisibleItem, visibleItemCount, totalItemCount));
-		boolean shouldAnimateItems = (mFirstVisibleItem != -1 && mLastVisibleItem != -1);
+    @Override
+    public final void setOnScrollListener(OnScrollListener l) {
+        // hijack the scroll listener setter and have this list also notify the additional listener
+        mAdditionalOnScrollListener = l;
+    }
 
-		int lastVisibleItem = firstVisibleItem + visibleItemCount - 1;
-		if (IS_AT_LEAST_HC && mIsScrolling && shouldAnimateItems) {
-			int indexAfterFirst = 0;
-			while(firstVisibleItem + indexAfterFirst < mFirstVisibleItem) {
-				Log.v(TAG, "Scrolled to reveal new item(s) ABOVE");
-				View item = view.getChildAt(indexAfterFirst);
-				doJazziness(item, firstVisibleItem+indexAfterFirst, -1);
-				indexAfterFirst++;
-			}
+    @Override
+    public final void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        Log.v(TAG, String.format("onScroll() - [firstVis: %d, visItemCount: %d, totItemCount: %d]", firstVisibleItem, visibleItemCount, totalItemCount));
+        boolean shouldAnimateItems = (mFirstVisibleItem != -1 && mLastVisibleItem != -1);
 
-			int indexBeforeLast = 0;
-			while(lastVisibleItem - indexBeforeLast > mLastVisibleItem) {
-				Log.v(TAG, "Scrolled to reveal new item(s) BELOW");
-				View item = view.getChildAt(lastVisibleItem - firstVisibleItem - indexBeforeLast);
-				doJazziness(item, lastVisibleItem, 1);
-				indexBeforeLast++;
-			}
-		}
+        int lastVisibleItem = firstVisibleItem + visibleItemCount - 1;
+        if (IS_AT_LEAST_HC && mIsScrolling && shouldAnimateItems) {
+            int indexAfterFirst = 0;
+            while (firstVisibleItem + indexAfterFirst < mFirstVisibleItem) {
+                Log.v(TAG, "Scrolled to reveal new item(s) ABOVE");
+                View item = view.getChildAt(indexAfterFirst);
+                doJazziness(item, firstVisibleItem + indexAfterFirst, -1);
+                indexAfterFirst++;
+            }
 
-		mFirstVisibleItem = firstVisibleItem;
-		mLastVisibleItem = lastVisibleItem;
-	}
+            int indexBeforeLast = 0;
+            while (lastVisibleItem - indexBeforeLast > mLastVisibleItem) {
+                Log.v(TAG, "Scrolled to reveal new item(s) BELOW");
+                View item = view.getChildAt(lastVisibleItem - firstVisibleItem - indexBeforeLast);
+                doJazziness(item, lastVisibleItem, 1);
+                indexBeforeLast++;
+            }
+        }
 
-	/**
-	 * Initializes the item view and triggers the animation.
-	 * @param item The view to be animated.
-	 * @param position The index of the view in the list.
-	 * @param scrollDirection Positive number indicating scrolling down, or negative number indicating scrolling up.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	private void doJazziness(View item, int position, int scrollDirection) {
-		if (mIsScrolling) {
-			ViewPropertyAnimator animator = com.nineoldandroids.view.ViewPropertyAnimator
-					.animate(item)
-					.setDuration(DURATION)
-					.setInterpolator(new AccelerateDecelerateInterpolator());
+        mFirstVisibleItem = firstVisibleItem;
+        mLastVisibleItem = lastVisibleItem;
 
-			int itemWidth = item.getWidth();
-			int itemHeight = item.getHeight();
-			scrollDirection = scrollDirection > 0 ? 1 : -1;
+        notifyAdditionalScrollListener(view, firstVisibleItem, visibleItemCount, totalItemCount);
+    }
 
-			switch (mEffect) {
-				case Standard:
-					break;
-				case Grow:
-					item.setPivotX(itemWidth / 2);
-					item.setPivotY(itemHeight / 2);
-					item.setScaleX(0.01f);
-					item.setScaleY(0.01f);
-					animator.scaleX(1).scaleY(1);
-					break;
-				case Curl:
-					item.setPivotX(0);
-					item.setPivotY(itemHeight / 2);
-					item.setRotationY(90);
-					animator.rotationY(0);
-					break;
-				case Wave:
-					item.setTranslationX(-itemWidth);
-					animator.translationX(0);
-					break;
-				case Flip:
-					item.setPivotX(itemWidth / 2);
-					item.setPivotY(itemHeight / 2);
-					item.setRotationX(-90 * scrollDirection);
-					animator.rotationXBy(90 * scrollDirection);
-					break;
-				case Helix:
-					item.setRotationY(180);
-					animator.rotationYBy(180 * scrollDirection);
-					break;
-				case Fan:
-					item.setPivotX(0);
-					item.setPivotY(0);
-					item.setRotation(70 * scrollDirection);
-					animator.rotationBy(-70 * scrollDirection);
-					break;
-				case Zipper:
-					boolean isEven = position % 2 == 0;
-					item.setTranslationX((isEven ? -1 : 1) * itemWidth);
-					animator.translationX(0);
-					break;
-				case Fade:
-					item.setAlpha(TRANSPARENT);
-					animator.setDuration(animator.getDuration()*5);
-					animator.alpha(OPAQUE);
-					break;
-				default:
-					break;
-			}
+    /**
+     * Initializes the item view and triggers the animation.
+     * @param item The view to be animated.
+     * @param position The index of the view in the list.
+     * @param scrollDirection Positive number indicating scrolling down, or negative number indicating scrolling up.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void doJazziness(View item, int position, int scrollDirection) {
+        if (mIsScrolling) {
+            ViewPropertyAnimator animator = com.nineoldandroids.view.ViewPropertyAnimator
+                    .animate(item)
+                    .setDuration(DURATION)
+                    .setInterpolator(new AccelerateDecelerateInterpolator());
 
-			animator.start();
-		}
-	}
+            int itemWidth = item.getWidth();
+            int itemHeight = item.getHeight();
+            scrollDirection = scrollDirection > 0 ? 1 : -1;
 
-	@Override
-	public void onScrollStateChanged(AbsListView view, int scrollState) {
-		switch(scrollState) {
-			case OnScrollListener.SCROLL_STATE_IDLE:
-				mIsScrolling = false;
-				break;
-			case OnScrollListener.SCROLL_STATE_FLING:
-			case OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
-				mIsScrolling = true;
-				break;
-			default: break;
-		}
+            switch (mEffect) {
+                case Standard:
+                    break;
+                case Grow:
+                    item.setPivotX(itemWidth / 2);
+                    item.setPivotY(itemHeight / 2);
+                    item.setScaleX(0.01f);
+                    item.setScaleY(0.01f);
+                    animator.scaleX(1).scaleY(1);
+                    break;
+                case Curl:
+                    item.setPivotX(0);
+                    item.setPivotY(itemHeight / 2);
+                    item.setRotationY(90);
+                    animator.rotationY(0);
+                    break;
+                case Wave:
+                    item.setTranslationX(-itemWidth);
+                    animator.translationX(0);
+                    break;
+                case Flip:
+                    item.setPivotX(itemWidth / 2);
+                    item.setPivotY(itemHeight / 2);
+                    item.setRotationX(-90 * scrollDirection);
+                    animator.rotationXBy(90 * scrollDirection);
+                    break;
+                case Helix:
+                    item.setRotationY(180);
+                    animator.rotationYBy(180 * scrollDirection);
+                    break;
+                case Fan:
+                    item.setPivotX(0);
+                    item.setPivotY(0);
+                    item.setRotation(70 * scrollDirection);
+                    animator.rotationBy(-70 * scrollDirection);
+                    break;
+                case Zipper:
+                    boolean isEven = position % 2 == 0;
+                    item.setTranslationX((isEven ? -1 : 1) * itemWidth);
+                    animator.translationX(0);
+                    break;
+                case Fade:
+                    item.setAlpha(TRANSPARENT);
+                    animator.setDuration(animator.getDuration() * 5);
+                    animator.alpha(OPAQUE);
+                    break;
+                default:
+                    break;
+            }
 
-		Log.v(TAG, String.format("isScrolling: %s", Boolean.valueOf(mIsScrolling)));
-	}
+            animator.start();
+        }
+    }
 
-	public void setTransitionEffect(TransitionEffect effect) {
-		mEffect = effect;
-	}
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        switch(scrollState) {
+            case OnScrollListener.SCROLL_STATE_IDLE:
+                mIsScrolling = false;
+                break;
+            case OnScrollListener.SCROLL_STATE_FLING:
+            case OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
+                mIsScrolling = true;
+                break;
+            default: break;
+        }
 
-	@SuppressWarnings("deprecation")
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-	private Point getScreenDimensions(Context context) {
-		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-		Display display = wm.getDefaultDisplay();
-		Point point = new Point();
-		if(IS_AT_LEAST_ICS) {
-			display.getSize(point);
-		} else {
-			point.set(display.getWidth(), display.getHeight());
-		}
-		return point;
-	}
+        Log.v(TAG, String.format("isScrolling: %s", Boolean.valueOf(mIsScrolling)));
+    }
+
+    public void setTransitionEffect(TransitionEffect effect) {
+        mEffect = effect;
+    }
+
+    private void notifyAdditionalScrollListener(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (mAdditionalOnScrollListener != null) {
+            mAdditionalOnScrollListener.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
+        }
+    }
 }
